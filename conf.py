04a078db910e99d51d77e699206dc90c47bb09e1
srujan shenoy <srujanshenoy@gmail.com>
2021-04-09 20:20:07 +0530

    accept decimal values ,restrict denominator 0

---
+++ conf.py
@@ -0,0 +1,23 @@
+print("N1/D1 =? N2/D2")
+X = float(input("N1: "))
+Y = float(input("D1: "))
+
+while Y == 0:
+    print("oops! Denominator can not be 0!")
+    X = float(input("N1: "))
+    Y = float(input("D1: "))
+
+Z = float(input("N2: "))
+W = float(input("D2: "))
+
+while W == 0:
+    print("oops! Denominator can not be 0!")
+    z = float(input("N2: "))
+    W = float(input("D2: "))
+
+P1 = X*W
+P2 = Y*Z
+if P1 == P2:
+    print("the fractions are equal.")
+else:
+    print("the fractiions are not equal.")
