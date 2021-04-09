585d1aa252c1ad86255fa3e695089e12c4a6fb69
srujanshenoy <80192419+srujanshenoy@users.noreply.github.com>
2021-04-09 20:38:55 +0530

    remove double numerator entering

--- conf.py
+++ conf.py
@@ -3,8 +3,7 @@ X = float(input("N1: "))
 Y = float(input("D1: "))
 
 while Y == 0:
-    print("oops! Denominator can not be 0!")
-    X = float(input("N1: "))
+    print("oops! Denominator can not be 0!)
     Y = float(input("D1: "))
 
 Z = float(input("N2: "))
@@ -12,7 +11,6 @@ W = float(input("D2: "))
 
 while W == 0:
     print("oops! Denominator can not be 0!")
-    z = float(input("N2: "))
     W = float(input("D2: "))
 
 P1 = X*W
